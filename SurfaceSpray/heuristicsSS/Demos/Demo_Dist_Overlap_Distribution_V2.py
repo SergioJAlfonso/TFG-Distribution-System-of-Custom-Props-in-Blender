cd0c5d1844421e9b56792bc8a8b3dfb22636e21b
SergioJAlfonso <sergialf@ucm.es>
2023-02-28 20:15:59 +0100

    Añadida la rotación a la distribución principal

--- SurfaceSpray/heuristicsSS/Demos/Demo_Dist_Overlap_Distribution_V2.py
+++ SurfaceSpray/heuristicsSS/Demos/Demo_Dist_Overlap_Distribution_V2.py
@@ -6,17 +6,17 @@ import numpy as np
 
 from enum import Enum
 
-from ..utilsSS.Actions import *
+from ...utilsSS.Actions import *
 from aima3.search import Problem as aimaProblem
-
 
 class ThresholdRandDistribution(aimaProblem):
 
     def __init__(self, rules_, bbox, initial, goal=None, ):
         super().__init__(initial, goal)
         self.rules = rules_
+        # self.noActionsLeft = False
         self.bounding_box = bbox
-        # Bounding box magnitudes
+        #Bounding box magnitudes
         self.half_bounding_size_x = (bbox[4][0] - bbox[0][0])/2.0
         self.half_bounding_size_y = (bbox[2][1] - bbox[0][1])/2.0
         self.half_bounding_size_z = (bbox[1][2] - bbox[0][2])/2.0
@@ -40,16 +40,15 @@ class ThresholdRandDistribution(aimaProblem):
 
         Returns True or False
         """
-        # Vertex B limits
-        max_B_X, min_B_X, max_B_Y, min_B_Y, max_B_Z, min_B_Z = self.getVertexBBoxLimits(
-            verB)
+        #Vertex B limits
+        max_B_X, min_B_X, max_B_Y, min_B_Y, max_B_Z, min_B_Z = self.getVertexBBoxLimits(verB)
 
-        # Overlap Condition
-        if (verA[0] >= min_B_X and verA[1] <= max_B_X and
+        #Overlap Condition
+        if(verA[0] >= min_B_X and verA[1] <= max_B_X and
            verA[2] >= min_B_Y and verA[3] <= max_B_Y and
            verA[4] >= min_B_Z and verA[5] <= max_B_Z):
             return True
-
+        
         return False
 
     def checkRestrictions(self, state, indexVertex):
@@ -60,17 +59,16 @@ class ThresholdRandDistribution(aimaProblem):
 
         Returns if all have passed. 
         """
-        # If vertex already in use
-        if (state.vertices_[indexVertex][2]):
+        #If vertex already in use
+        if(state.vertices_[indexVertex][2]):
             return False
 
-        # Vertex we potentially want to place an object on it.
+        #Vertex we potentially want to place an object on it.
         pCandidate = state.vertices_[indexVertex][0]
-
-        if (not self.rules.overlap):
-            # Vertex A limits
-            max_X, min_X, max_Y, min_Y, max_Z, min_Z = self.getVertexBBoxLimits(
-                pCandidate)
+   
+        if(not self.rules.overlap):
+            #Vertex A limits
+            max_X, min_X, max_Y, min_Y, max_Z, min_Z = self.getVertexBBoxLimits(pCandidate)
 
             vertex_bbox_limits = (max_X, min_X, max_Y, min_Y, max_Z, min_Z)
 
@@ -78,25 +76,23 @@ class ThresholdRandDistribution(aimaProblem):
         satisfiesRestrictions = True
 
         while (i < len(state.actionsApplied_) and satisfiesRestrictions == True):
-            # Access vertices that has an object on it.
+            #Access vertices that has an object on it. 
             indexVertex = state.actionsApplied_[i].indexVertex
             vertexInUse = state.vertices_[indexVertex][0]
 
-            # Check bounding box overlap if needed
-            if (not self.rules.overlap):
-                satisfiesRestrictions = not self.boundingBoxOverlapping(
-                    vertex_bbox_limits, vertexInUse)
+            #Check bounding box overlap if needed
+            if(not self.rules.overlap):
+                satisfiesRestrictions = not self.boundingBoxOverlapping(vertex_bbox_limits, vertexInUse)
 
-            # Calculates distance between 2 tridimensional points.
-            distance = math.sqrt((vertexInUse[0]-pCandidate[0])**2 + (
-                vertexInUse[1]-pCandidate[1])**2 + (vertexInUse[2]-pCandidate[2])**2)
-            # Compares if distance is lesser or equals to minimum distance.
+            #Calculates distance between 2 tridimensional points.
+            distance = math.sqrt((vertexInUse[0]-pCandidate[0])**2 + (vertexInUse[1]-pCandidate[1])**2 + (vertexInUse[2]-pCandidate[2])**2)
+            #Compares if distance is lesser or equals to minimum distance.
             satisfiesRestrictions = satisfiesRestrictions and distance >= self.rules.distance_between_items
 
-            i += 1
+            i+= 1
 
         return satisfiesRestrictions
-
+    
     def actions(self, state):
         """
         Defines possible vertices in current state.
@@ -105,28 +101,28 @@ class ThresholdRandDistribution(aimaProblem):
 
         Returns an array of indices, indicating which indices you can place an object on. 
         """
-        possibleActions = []
+        possibleActions = [] 
 
         sizeV = len(state.vertices_)
 
-        # Iterate over each state vertex checking if this vertex has a object on it, otherwise
-        # it'll be considered as an action. (An object can be placed on it)
+        #Iterate over each state vertex checking if this vertex has a object on it, otherwise 
+        # it'll be considered as an action. (An object can be placed on it) 
         # remaining = self.goal.objectsPlaced_ - state.objectsPlaced_
         remaining = 1
 
         randomIndices = random.sample(range(sizeV), sizeV)
         j = 0
-        while (remaining > 0 and j < sizeV):
+        while(remaining > 0 and j < sizeV):
             i = randomIndices[j]
-            if (self.checkRestrictions(state, i) == True):
+            if(self.checkRestrictions(state, i) == True):
                 j = 0
-                # Check that is not a vertex that is already used
-                while (j < len(possibleActions) and (possibleActions[j].indexVertex != i)):
-                    j += 1
+                #Check that is not a vertex that is already used 
+                while ( j < len(possibleActions) and (possibleActions[j].indexVertex != i)):
+                    j+= 1
 
-                if (j < len(possibleActions) or len(possibleActions) == 0):
+                if(j < len(possibleActions) or len(possibleActions) == 0 ):
                     remaining -= 1
-                    action = Actions(i, self.random_step_rotation())
+                    action = Actions(i, (0,0,0))
                     possibleActions.append(action)
 
             j += 1
@@ -135,30 +131,6 @@ class ThresholdRandDistribution(aimaProblem):
             print('Could not find any vertex that satifies all rules.')
 
         return possibleActions
-
-    def random_step_rotation(self):
-        precision = 100.0
-
-        rang_x = (int)(self.rules.rotation_range[0] * precision)
-        rang_y = (int)(self.rules.rotation_range[1] * precision)
-        rang_z = (int)(self.rules.rotation_range[2] * precision)
-
-        step_x = (int)(self.rules.rotation_steps[0] * precision)
-        step_y = (int)(self.rules.rotation_steps[1] * precision)
-        step_z = (int)(self.rules.rotation_steps[2] * precision)
-
-        rot_x = 0
-        rot_y = 0
-        rot_z = 0
-
-        if (self.rules.rotations[0] != 0):
-            rot_x = random.randrange(-rang_x, rang_x, step_x)
-        if (self.rules.rotations[1] != 0):
-            rot_y = random.randrange(-rang_y, rang_y, step_y)
-        if (self.rules.rotations[2] != 0):
-            rot_z = random.randrange(-rang_z, rang_z, step_z)
-
-        return ((rot_x/precision) * (math.pi/180.0), rot_y/precision * (math.pi/180.0), rot_z/precision * (math.pi/180.0))
 
     def result(self, state, action):
         """
@@ -174,9 +146,9 @@ class ThresholdRandDistribution(aimaProblem):
         """
         Returns if the number of placed objects so far is equal to number of objects defined as goal.
         """
-        # numero de objetos pedidos, separacion de objetos, tal.
+        #numero de objetos pedidos, separacion de objetos, tal.
 
-        # Chill down
+        #Chill down 
         return state.objectsPlaced_ == self.goal.objectsPlaced_
         # return True
 
